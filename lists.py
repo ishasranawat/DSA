228302aa247f06c416b81d25147933fcfbffd02d
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-27 11:01:03 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -8,3 +8,22 @@ print(list[1])
 for i in list:
   print(i)
 # op- a, b, c
+
+
+# insert
+list.insert(0,'f')
+print(list)
+# ['f', 'a', 'b', 'c']
+
+
+# append
+list.append('j')
+print(list)
+# ['f', 'a', 'b', 'c', 'j']
+
+
+# extend
+newlist= ['w','p','o']
+list.extend(newlist)
+print(list)
+# ['f', 'a', 'b', 'c', 'j', 'w', 'p', 'o']
