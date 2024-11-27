8adcbadc8492cfee56a6ecc0d98ce8b49691f240
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-27 10:43:50 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -15,7 +15,13 @@ print(a1)
  # output- 1 2 3 4 5 6
 
 # remove element
+# 1 using pop (give index)
 a1.pop(2)
 print(a1)
 # output- 1 2 4 5 6
+
+# 2 using remove(give the value)
+a1.remove(5)
+print(a1)
+# output array('i', [1, 2, 3, 4, 6])
 
