bb18eb32062d6c81b57ca206b0978c36a818c84d
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-28 14:23:49 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -50,3 +50,51 @@ my_dict.clear()
 print(my_dict)
 # OP {}
 
+          ### Dictionary Methods ###
+my_dict= {
+    'apple':'sweet',
+    'orange':'sour',
+    'watermelon':{'one':"1",'two':'2','three':'3'}
+}
+
+# 1.copy
+my_dict.copy()
+print(my_dict)
+# OP {'apple': 'sweet', 'orange': 'sour', 'watermelon': {'one': '1', 'two': '2', 'three': '3'}}
+
+
+# 2.get
+op=my_dict.get('apple')
+print(op)
+# OP sweet
+
+
+# 3.list items
+dict_itmes=my_dict.items()
+print(dict_itmes)
+#  OP dict_items([('apple', 'sweet'), ('orange', 'sour'), ('watermelon', {'one': '1', 'two': '2', 'three': '3'})])
+
+
+
+# 4. list keys
+dict_keys= my_dict.keys()
+print(dict_keys)
+#  OP dict_keys(['apple', 'orange', 'watermelon'])
+
+
+
+# 5. set pair
+dict_setdefault= my_dict.setdefault('lemon','zesty')
+print(my_dict)
+#OP  {'apple': 'sweet', 'orange': 'sour', 'watermelon': {'one': '1', 'two': '2', 'three': '3'}, 'lemon': 'zesty'}
+
+# 6.Update (add a new dictionary to existing)
+new_dict= {
+    'peas':'green',
+    'raddish':'white',
+}
+dict_update= my_dict.update(new_dict)
+print(my_dict)
+# OP {'apple': 'sweet', 'orange': 'sour', 'watermelon': {'one': '1', 'two': '2', 'three': '3'}, 'lemon': 'zesty', 'peas': 'green', 'raddish': 'white'}
+
+
