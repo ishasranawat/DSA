6631ffa904ee00b589cdfc3fea955ce096f1cc7e
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-28 11:30:49 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -8,14 +8,26 @@ print(my_dict)
 # to get the keys
 print(my_dict.keys())
 
+
 # adding a pair
 my_dict['strawberry']='pretty'
 print(my_dict)
+
 
 # traversal 
 def print_dict(my_dict):
     for key in my_dict:
         print(key, my_dict[key])
-
 print_dict(my_dict)
+
+
+# searching 
+def search(my_dict, value):
+    for key in my_dict:
+        if my_dict[key] == value:
+            return key 
+    return -1  
+print(search(my_dict, 'sweet'))  # Output: apple
+print(search(my_dict, 'apple'))  # Output: -1 
+print(search(my_dict, {'one': "1", 'two': '2', 'three': '3'}))  # Output: watermelon
 
