d70b25e363d123f054e4eaaa13eb63996565d3ac
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-28 11:44:05 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -31,3 +31,22 @@ print(search(my_dict, 'sweet'))  # Output: apple
 print(search(my_dict, 'apple'))  # Output: -1 
 print(search(my_dict, {'one': "1", 'two': '2', 'three': '3'}))  # Output: watermelon
 
+
+# **removing pairs**
+
+my_dict.pop('apple')
+print(my_dict)    
+# OP {'orange': 'sour', 'watermelon': {'one': '1', 'two': '2', 'three': '3'}}
+
+my_dict.popitem()
+print(my_dict)
+# OP {'apple': 'sweet', 'orange': 'sour'}
+
+del my_dict['watermelon']
+print(my_dict)
+# OP {'apple': 'sweet', 'orange': 'sour'}
+
+my_dict.clear()
+print(my_dict)
+# OP {}
+
