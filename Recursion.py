f8f78328d98886908a5d3934599a0279819c548f
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-30 16:11:36 +0530

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -96,6 +96,14 @@ arr= ['car', 'taco', 'banana']
 print(capitalizeFirst(arr))
 
 
+# CAPITALIZE WORDS SOLUTION
+def capitalizeWords(arr):
+    result = []
+    if len(arr) == 0:
+        return result
+    result.append(arr[0].upper())
+    return result + capitalizeWords(arr[1:])
+
 
 
 
