3ba81b55e940adf0d8e13937880ddd3a52296b5f
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-01 16:23:03 +0530

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -12,6 +12,7 @@ class LinkedList:
         self.length = 1
         
 
+
 2.# Insertion at the beginning
 class Node:
     def __init__(self, value):
@@ -28,4 +29,50 @@ class LinkedList:
         new_node.next=self.head
         self.head=new_node
         self.length+=1
+
+
+
+3.# Insertion at the end 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head= new_node
+            self.tail= new_node
+            self.length+=1 
+        else:
+            self.tail.next= new_node
+            self.tail=new_node
+            self.length+=1
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             
