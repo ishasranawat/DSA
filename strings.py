3de2af891e4eed3f45796d1ad4d513b40c1a2f3e
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-01 11:39:38 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -8,3 +8,24 @@ def reverseString(str: str) -> str:
     
     # Join the words with a single space
     return ' '.join(words)
+
+
+
+# Palindrome 
+
+def is_palindrome(s):
+    # Convert to lowercase and remove spaces
+    s = s.lower().replace(" ", "")
+    
+    # Check if the string is equal to its reverse
+    if len(s)<=2:
+        return "false"
+    return "true" if s == s[::-1] else "false"
+       
+
+# Test the function
+str = "abcdcba"
+result = is_palindrome(str)
+print(result)
+
+
