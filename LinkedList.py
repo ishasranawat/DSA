293249f7cc9c275b988ac31685c118254ec4cb08
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-02 21:47:25 +0530

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -136,6 +136,18 @@ class LinkedList:
         self.tail = current_node
 
 
+6. #Reverse a LL
+def reverse(self):
+    prev_node = None
+    current_node = self.head
+    while current_node is not None:
+        next_node = current_node.next
+        current_node.next = prev_node
+        prev_node = current_node
+        current_node = next_node
+    self.head, self.tail = self.tail, self.head
+
+
 
 
 
