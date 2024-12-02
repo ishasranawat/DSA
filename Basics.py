ac69a6312767816db1a0896f0f72c8a71b0aae37
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-03 01:32:48 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,15 +1,15 @@
-# Creating Array
+1.# Creating Array
 from array import *
 a1= array('i', [1,2,3,4,5,6])
 
 
-# Insertion 
+2.# Insertion 
 a1.insert(1, 7)
 print(a1)    
 # output - (1,7,2,3,4,5,6)
 
 
-# Traversal 
+3.# Traversal 
  for i in a1:
    print(i)
  # output- 1 2 3 4 5 6
