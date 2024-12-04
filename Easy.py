955d1e180c36c70b9c8732abb4089e51d1cf2c98
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-05 03:42:40 +0530

    Update Easy.py

--- Easy.py
+++ Easy.py
@@ -8,3 +8,15 @@ def pattern(n):
 
 n=int(input())
 pattern(n)
+
+def is_prime(n):
+    # Numbers less than 2 are not prime
+    if n < 2:
+        return False
+    
+    # Check for divisibility up to the square root of n
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
