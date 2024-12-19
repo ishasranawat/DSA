e086f15aa335c821d46342a83ec7c950067034ea
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-19 10:32:09 +0530

    Update Arrays_questions.py

--- Arrays_questions.py
+++ Arrays_questions.py
@@ -7,7 +7,6 @@ def missing_number(arr, n):
     return missing
 
 
-
 2.# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. (Two sum)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
