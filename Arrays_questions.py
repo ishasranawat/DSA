2a1604a3745826dcd0f2ac763e8961e7ca360e3a
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-02-01 18:27:10 +0530

    Update Arrays_questions.py

--- Arrays_questions.py
+++ Arrays_questions.py
@@ -90,8 +90,7 @@ def contains_duplicate(nums):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i]== nums[j]:
-                return True
-            
+                return True         
     return False
                 
 nums= [1,2,3,1]
